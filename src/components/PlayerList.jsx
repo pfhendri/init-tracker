aa2cd782e1c0d8d7b752794720665efceeb2787d
Phil Hendricks <pfhendri1@gmail.com>
2020-02-15 14:33:56 -0600

    added multi-list

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -6,11 +6,15 @@ export default function PlayerList(props) {
   const [newPlayer, setNewPlayer] = useState({ id: null, name: '', init: 0 })
   const { openView } = props
   const {
-    list, 
+    currentList,
+    lists, 
     addPlayer, 
     updatePlayer, 
-    removePlayer, 
-    clearList, 
+    removePlayer,
+    isNewList,
+    setCurrentList,
+    updateList, 
+    removeList, 
     saveList
   } = useContext(ListContext)
 
@@ -25,9 +29,40 @@ export default function PlayerList(props) {
     setNewPlayer({ name: value, init: 0 })
   }
 
+  const handleListNameChange = (e) => {
+    const { value } = e.target
+    const foundList = lists.find(list => list.name === value)
+    if (foundList) {
+      setCurrentList(foundList.id)
+    } else {
+      updateList(value)
+    }
+  }
+
+  const handleListSelect = (e) => {
+    const id = parseInt(e.target.value)
+    console.log(id)
+    setCurrentList(id)
+  }
+
   return(
     <div className='PlayerList'>
       <h1>Player List</h1>
+      { isNewList ? <div>
+        <input type='text' list='playerLists' placeholder='Enter new list name, or select one' onChange={handleListNameChange} style={{ width: '100%', marginBottom: '.5rem', padding: '.5rem'}}/>
+        <datalist id='playerLists'>
+          {lists.map((list, i) => (
+            <option value={list.name} key={list.id}/>
+          ))}
+        </datalist>
+      </div> : 
+      <select defaultValue={currentList.id} onChange={handleListSelect} style={{ width: '100%', marginBottom: '.5rem', padding: '.5rem', border: '2px solid #eee'}}>
+        <option value=''>New List</option>
+        {lists.map((list, i) => (
+          <option value={list.id} key={list.id}>{list.name}</option>
+        ))}
+      </select> }
+      
       <form onSubmit={handleSubmit} className='new-player-form'>
         <input
           type='text'
@@ -40,7 +75,7 @@ export default function PlayerList(props) {
       </form>
       <hr />
       <ul className='player-list'>
-        {list.map( (player, i) => (
+        {currentList.players.map( (player, i) => (
           <li key={player.name} className='player-item'>
             {player.name} 
             <input 
@@ -54,7 +89,7 @@ export default function PlayerList(props) {
       </ul>
       <div className='button-actions'>
         <button type='button' className='btn' onClick={saveList}>Save List</button>
-        <button type='button' className='btn' onClick={clearList}>Clear List</button>
+        <button type='button' className='btn' onClick={removeList}>Remove List</button>
       </div>
       <hr />
       <div style={{display: 'flex', justifyContent: 'center' }}>
--- src/contexts/ListContext.jsx
+++ src/contexts/ListContext.jsx
@@ -1,47 +1,88 @@
 import React, { useState, useEffect } from 'react'
 
+import { generateId } from '../util'
+
 export const ListContext = React.createContext()
 
 export const ListProvider = (props) => {
-  const [list, setList] = useState([])
+  const [currentList, setList] = useState({ id: generateId(), name: '', players: [] })
+  const [isNewList, setNewList] = useState(true)
+
+  const [lists, setLists] = useState([])
 
   useEffect(() => {
-    const savedList = localStorage.getItem('currentList')
-    if(savedList){
-      setList(JSON.parse(savedList))
+    const savedLists = localStorage.getItem('savedLists')
+    if(savedLists){
+      setLists(JSON.parse(savedLists))
     }
   }, [])
 
   const addPlayer = (newPlayer) => {
-    newPlayer.id = Math.floor(Date.now())
-    const updatedList = [...list, newPlayer]
+    newPlayer.id = generateId()
+    const updatedList = {...currentList, players: [ ...currentList.players, newPlayer] }
     setList(updatedList)
   }
 
   const updatePlayer = (name, value, player) => {
     const updatedPlayer = { ...player, [name]: value }
-    const playerList = list.slice()
+    const playerList = currentList.players.slice()
     const playerIndex = playerList.findIndex(({id}) => id === player.id)
     playerList[playerIndex] = updatedPlayer
-    setList(playerList)
-  }
-
-  const removePlayer = (id) => {
-    const updatedList = list.filter(player => player.id !== id)
+    const updatedList = {...currentList, players: playerList}
     setList(updatedList)
   }
 
+  const removePlayer = (id) => {
+    const updatedList = currentList.filter(player => player.id !== id)
+    setList(updatedList)
+  }
+
+  const newList = () => {
+    setList({ id: generateId(), name: '', players: []})
+    setNewList(true)
+  }
+
+  const updateList = (value) => {
+    const updatedList = {...currentList, name: value}
+    setList(updatedList)
+  }
+
+  const setCurrentList = (id) => {
+    const list = lists.find(li => li.id === id)
+    if (list) {
+      setList(list)
+      setNewList(false)
+    } else {
+      newList()
+    }
+  }
+
   const clearList = () => {
-    setList([])
-    localStorage.removeItem('currentList')
+    setList({name: '', players: []})
+  }
+
+  const removeList = () => {
+    const updatedLists = lists.filter(list => list.id !== currentList.id)
+    setLists(updatedLists)
+    localStorage.setItem('savedLists', JSON.stringify(updatedLists))  
+    newList()
   }
 
   const saveList = () => {
-    localStorage.setItem('currentList', JSON.stringify(list))
+    const updatedLists = [...lists]
+    const currentListIndex = updatedLists.findIndex(list => list.id === currentList.id)
+    if (currentListIndex >= 0) {
+      updatedLists[currentListIndex] = currentList
+    } else {
+      updatedLists.push(currentList)
+    }
+    setLists(updatedLists)
+    localStorage.setItem('savedLists', JSON.stringify(updatedLists))
+    setNewList(false)
   }
 
   return(
-    <ListContext.Provider value={ { list, addPlayer, updatePlayer, removePlayer, clearList, saveList } }>
+    <ListContext.Provider value={ { isNewList, currentList, addPlayer, updatePlayer, removePlayer, lists, newList, setCurrentList, updateList, clearList, removeList, saveList } }>
       {props.children}
     </ListContext.Provider>
   )
