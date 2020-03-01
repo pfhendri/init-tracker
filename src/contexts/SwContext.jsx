f03942384e71b49582a1b9d30e4bce103e1ba90c
Phil Hendricks <pfhendri1@gmail.com>
2020-03-01 16:03:48 -0600

    added ask for install

--- src/contexts/SwContext.jsx
+++ src/contexts/SwContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Notification from '../components/Notifications/Notification'
 
@@ -10,6 +10,39 @@ export const SwContext = React.createContext(initialContext)
 
 export const SwProvider = ({children}) => {
   const [notification, setNotification] = useState(null)
+  const [installPrompt, setInstallPrompt] = useState(null)
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      console.log('Install Prompt Fired')
+
+      setInstallPrompt(e)
+
+      if((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || window.navigator.standalone === true){
+        return false;
+      }
+      
+      if(installPrompt){
+        setNotification(<div>Would you like to install this App on your device? <button className='btn btn-negative' onClick={installApp}>Install</button></div>)
+      }
+    })
+  }, [])
+
+  const installApp = async () => {
+    if(!installPrompt) return false
+
+    installPrompt.prompt()
+    let outcome = await this.installPrompt.userChoice
+    if(outcome.outcome === 'accepted'){
+      console.log("App Installed")
+    }
+    else{
+      console.log("App not installed");
+    }
+
+    setInstallPrompt(null)
+  }
 
   const handleNotification = (text) => {
     setNotification(text) 
--- src/index.js
+++ src/index.js
@@ -8,17 +8,20 @@ import { AppProvider } from './contexts/AppContext'
 import { SwContext } from './contexts/SwContext'
 
 const ServiceWorkerApp = () => {
-  const { handleNotification } = React.useContext(SwContext)
-
-  
+  const { handleNotification } = React.useContext(SwContext)  
 
   React.useEffect( () => {
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
     // Learn more about service workers: https://bit.ly/CRA-PWA
     serviceWorker.register({
-      onSuccess: (reg) => { handleNotification('Successfully Installed') },
-      onUpdate: (reg) => { handleNotification(<div>App ready to update! <button class='btn btn-negative' onClick={() => window.location.reload()}>Update</button></div>) }
+      onSuccess: (reg) => { 
+        console.log(reg)
+        handleNotification('App is ready to be installed!') 
+      },
+      onUpdate: (reg) => { 
+        handleNotification(<div>App ready to update! <button class='btn btn-negative' onClick={() => window.location.reload()}>Update</button></div>) 
+      }
     });
   }, [])
 
